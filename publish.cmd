a0f3030c4de63046d4d2a2755ce119525987f9b4
Jinjie Zhou <jinjiez@microsoft.com>
2018-09-24 16:57:01 +0800

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=botluisdemotest-Web-Deploy.pubxml -p:Password=9TGfYcAXnZMMP4s0gmSb7yjRG1eamTEznBuz56zgeXPWtpzlP5r6Crcvvi1b
+
