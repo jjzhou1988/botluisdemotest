8b87358ea1b488156616f515bdebb8eee03ceec7
Jinjie Zhou <jinjiez@microsoft.com>
2018-09-25 22:54:50 +0800

    Test by adding Context.Forward()

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Threading;
+using Microsoft.Bot.Connector;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -31,12 +33,30 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
             await this.ShowLuisResult(context, result);
+
+            IMessageActivity messageToForward = Activity.CreateMessageActivity();
+            messageToForward.Text = "stop!";
+            messageToForward.TextFormat = "plain";
+            messageToForward.Locale = "en-Us";
+
+            await context.Forward(this, AfterFAQDialog, messageToForward, CancellationToken.None);
+        }
+
+        private async Task AfterFAQDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            var messageHandled = await result;
+            if (messageHandled!=null)
+            {
+                await context.PostAsync("Sorry, I wasn't sure what you wanted.");
+            }
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Cancel")]
         public async Task CancelIntent(IDialogContext context, LuisResult result)
         {
             await this.ShowLuisResult(context, result);
+            context.Done(result);
         }
 
         [LuisIntent("Help")]
